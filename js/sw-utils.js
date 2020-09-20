31a950c805c08b2203547bd16b4591070956014f
Luis Carlos Gutiérrez Gutiérrez <luis.gutierrez@sofka.com.co>
2020-09-20 09:57:35 -0500

    programa

---
+++ js/sw-utils.js
@@ -0,0 +1,10 @@
+const actualizarCacheDinamico = (dynamicCache,request,response) => {
+    if(response.ok){
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(request,response.clone())
+            return response.clone()
+        })
+    } else {
+        return response;
+    }
+}
---
+++ sw.js
@@ -0,0 +1,58 @@
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE = 'static-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
+
+
+// LO DE UNO
+const APP_SHELL_STATIC = [
+    '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js'
+];
+
+// LIBRERIAS EXTERNAS
+const APP_SHELL_INMUTABLE = [
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+    'https://fonts.googleapis.com/css?family=Lato:400,300',
+    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'css/animate.css',
+    'js/libs/jquery.js'
+];
+
+self.addEventListener('install', event => {
+    const static_cache = caches.open(STATIC_CACHE).then(cache => cache.addAll(APP_SHELL_STATIC));
+    const inmutable_cache = caches.open(INMUTABLE_CACHE).then(cache => cache.addAll(APP_SHELL_INMUTABLE));
+    const promesasCache = Promise.all([static_cache, inmutable_cache]);
+    event.waitUntil(promesasCache);
+});
+
+self.addEventListener('activate', event => {
+    const respuesta = caches.keys().then(keys => {
+        keys.forEach(key => {
+            if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key)
+            }
+        })
+    })
+    event.waitUntil(respuesta);
+})
+
+self.addEventListener('fetch', event => {
+    caches.match(event.request).then(resp => {
+        if (resp) { return resp }
+        else {
+            return fetch(event.request).then(newResp => actualizarCacheDinamico(DYNAMIC_CACHE, event.request, newResp))
+        }
+    })
+})
+
